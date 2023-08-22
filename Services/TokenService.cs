8f18a82394fde0a33d36ace914d1751e9b7d8565
Marcelo <mshf15@hotmail.com>
2023-08-22 09:17:32 -0300

    003 - Criacao de authenticacao jwt

---
+++ Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using apiPagamento.Context;
+using apiPagamento.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace apiPagamento.Services
+{
+    public class TokenService
+    {
+        public static object GerarToken(Usuario usuario)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(Key.secret);
+            SecurityTokenDescriptor tokenConfig = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim("UsuarioId", usuario.Id.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenConfig);
+            string tokenString = tokenHandler.WriteToken(token);
+
+            return new
+            {
+                Token = tokenString
+            };
+        }
+    }
+}
